a7efc21dd9b99c207f0e34e64656a27eb0f6e2ff
michael <michael@maxkit.com.tw>
2015-06-21 09:56:20 +0800

    use requires and google analysis

---
+++ build.js
@@ -0,0 +1,9 @@
+({
+    baseUrl: ".",
+    paths: {
+        jquery: "jquery-1.11.3.min"
+    },
+    excludeShallow : [ 'jquery' ],
+    name: "mytos_main",
+    out: "mytos_app-built.js"
+})
---
+++ mytos_main-built.js
@@ -0,0 +1,9 @@
+require.config({
+    paths : {
+        'jquery' : 'jquery-1.11.3.min'
+    }
+});
+
+require(['mytos_app-built'], function(MyTOS){
+
+});
